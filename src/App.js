b1209eb03583adedfc22632b9eca188bc234f732
erfan <erfanajoglu098@gmail.com>
2023-11-02 22:30:58 +0330

    Reviews component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Reviews from "./Reviews";
 
 function App() {
   return (
-    <div>
-      <h1>H</h1>
+    <div className="flex flex-col justify-center items-center my-auto h-screen bg-slate-200">
+      <h1 className="text-3xl font-extrabold my-6">Our Reviews</h1>
+      <div className="w-32 h-1 bg-blue-800"></div>
+      <Reviews />
     </div>
   );
 }
